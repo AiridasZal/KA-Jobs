5248231697e71fb98186b6e80a2fc1f250073cdc
norbertas156 <norbertas156@gmail.com>
2022-01-13 17:35:13 +0200

    Profile page

--- profile.php
+++ profile.php
@@ -12,9 +12,11 @@
 <main class="main">
         <!--=============== HOME ===============-->
             <section class="home section" id="home">
-                <div id="profile">
-                    <img class="profileIMG" src="img/profile.png" style="width: 200px; position: relative;">
-                    <?php
+                <div class="container profile_container" id="profile">
+                    <div class="user_information">
+
+                        <img class="profileIMG" src="img/profile.png" style="width: 200px; position: relative;">
+                        <?php
                         $sql = "SELECT * FROM users WHERE usersId=$_SESSION[userid]";
                         $stmt = mysqli_stmt_init($conn);
                         if (!mysqli_stmt_prepare($stmt, $sql)) {
@@ -28,11 +30,11 @@
                         while($data = $result->fetch_assoc()){
                             echo 
                             "<h1 style='text-align: center;'>
-                                {$data['usersName']}
+                            {$data['usersName']}
                             </h1>";
                             echo 
                             "<p style='text-align: center; font-size: 20px;'>
-                                {$data['usersUid']}
+                            {$data['usersUid']}
                             </p>";
                             echo 
                             "<p style='text-align: center; font-size: 20px;'>
@@ -40,45 +42,45 @@
                             </p>";
                         }
                     ?>
+                </div>
+                <div class="change_settings">
+
                     <div id="title"><h1>Profile settings<h1></div>
-                    <div id="passwordChange">
-                        <h1 class="spacing">Change password</h1>
-                        <form action="includes/changePWD.inc.php" method="post">
-
-                            <div class="form__div">
-                                <input type="password" class="form__input" name="newPassword" placeholder=" ">
-                                <label for="" class="form__label">Password</label>
-                            </div>
-
-                            <div class="form__div">
-                                <input type="password" class="form__input" name="repNewPwd" placeholder=" ">
-                                <label for="" class="form__label">New Password</label>
-                            </div>
-
-                            <input type="submit" class="button" name="submit" value="Change">
-                        </form>
-                        <?php
-                        if (isset($_GET["error"])) {
-                            if ($_GET["error"] == "emptyinput") {
-                                echo '<p class=errormessage style="text-align: center; color: red;">Fill in all fields!</p>';
-                            }
-                            else if($_GET["error"] == "passwordsdontmatch"){
-                                echo '<p class=errormessage style="text-align: center; color: red;">Passwords do not match!</p>';
-                            }
-                            else if($_GET["error"] == "none"){
-                                echo '<p class=errormessagenone style="text-align: center; color: green;">Password changed successfully!</p>';
-                            }
-                        }
-                        ?>
-                    </div>
-                    
                     <div id="emailChange">
-                        <h1 class="spacing">Change E-Mail</h1>
                         <form action="includes/changeEmail.inc.php" method="post">
+                            <div class="label_grid">
+
+                                <div class="form__div">
+                                    <input type="" class="form__input" name="newEmail" placeholder=" ">
+                                    <label for="" class="form__label">Name</label>
+                                </div>
+                                <div class="form__div">
+                                    
+                                    <input type="" class="form__input" name="newEmail" placeholder=" ">
+                                    <label for="" class="form__label">Last name</label>
+                                </div>
+                            </div>
                             <div class="form__div">
-                                <input type="password" class="form__input" name="newEmail" placeholder=" ">
+                                <input type="" class="form__input" name="newEmail" placeholder=" ">
                                 <label for="" class="form__label">New Email</label>
                             </div>
+                            <div class="form__div">
+                                <input type="" class="form__input" name="newEmail" placeholder=" ">
+                                <label for="" class="form__label">Phone number</label>
+                            </div>
+                            <div class="label_grid">
+
+                            <div class="form__div ">
+                                    <input type="" class="form__input" name="newEmail" placeholder=" ">
+                                    <label for="" class="form__label">Country</label>
+                                </div>
+
+                                <div class="form__div">
+                                    
+                                    <input type="" class="form__input" name="newEmail" placeholder=" ">
+                                    <label for="" class="form__label">City</label>
+                                </div>
+                                </div>
                             <input type="submit" class="button" name="submit" value="Change">
                         </form>
                         <?php
@@ -98,11 +100,43 @@
                         }
                         ?>
                     </div>
+                    <div id="passwordChange">
+                        <h1 class="spacing">Change password</h1>
+                        <form action="includes/changePWD.inc.php" method="post">
+                            
+                            <div class="form__div">
+                                <input type="password" class="form__input" name="newPassword" placeholder=" ">
+                                <label for="" class="form__label">Password</label>
+                            </div>
+                            
+                            <div class="form__div">
+                                <input type="password" class="form__input" name="repNewPwd" placeholder=" ">
+                                <label for="" class="form__label">New Password</label>
+                            </div>
+                            
+                            <input type="submit" class="button" name="submit" value="Change">
+                        </form>
+                        <?php
+                        if (isset($_GET["error"])) {
+                            if ($_GET["error"] == "emptyinput") {
+                                echo '<p class=errormessage style="text-align: center; color: red;">Fill in all fields!</p>';
+                            }
+                            else if($_GET["error"] == "passwordsdontmatch"){
+                                echo '<p class=errormessage style="text-align: center; color: red;">Passwords do not match!</p>';
+                            }
+                            else if($_GET["error"] == "none"){
+                                echo '<p class=errormessagenone style="text-align: center; color: green;">Password changed successfully!</p>';
+                            }
+                        }
+                        ?>
+                    </div>
+                    
+                </div>
                 </div>
             </section>
-</main>
+        </main>
 
 <?php
-    include_once 'footer.php';
-?>
+    include_once 'footer.signup.php';
+    ?>
 
